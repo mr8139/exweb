a6a038f7d0f47127cbb145bce0dd91218e464ec8
mr8139 <michalr@orotmodiin.co.il>
2026-01-20 16:18:01 +0200

    Initial commit

---
+++ app/src/main/java/michal/rapaport/exweb/MainActivity.java
@@ -0,0 +1,34 @@
+
+package michal.rapaport.exweb;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv=findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        String stringUrl = "https://www.inn.co.il/";
+        wv.loadUrl(stringUrl);
+        wv.setWebViewClient(new MyWebClientbyViewClient());
+    }
+
+    private class MyWebClientbyViewClient extends WebViewClient {
+       public boolean shouldOverideUrlLoading(WebView view,String url)
+       {
+           view.loadUrl(url);
+           return true;
+       }
+    }
+}
